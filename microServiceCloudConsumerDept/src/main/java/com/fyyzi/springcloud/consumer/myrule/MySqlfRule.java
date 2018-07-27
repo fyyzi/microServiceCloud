2b5b50c171b061173ab174c333c04d703bd53f24
xiyang_homePC <xiyang_job@126.com>
2018-07-27 23:59:25 +0800

    update

---
+++ microServiceCloudConsumerDept/src/main/java/com/fyyzi/springcloud/consumer/myrule/MySqlfRule.java
@@ -0,0 +1,18 @@
+package com.fyyzi.springcloud.consumer.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySqlfRule {
+
+    @Bean
+    public IRule getIRule(){
+
+        return new RandomRule();
+
+    }
+
+}
