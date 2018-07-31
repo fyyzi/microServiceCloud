96191760f7b2d302be5d65bdd61154ae005ce75f
xiyang_homePC <xiyang_job@126.com>
2018-08-01 00:22:00 +0800

    1

---
+++ microServiceCloudApi/src/main/java/com/fyyzi/springcldou/service/DeptClientService.java
@@ -0,0 +1,23 @@
+package com.fyyzi.springcldou.service;
+
+import com.fyyzi.springcldou.entities.Dept;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@FeignClient(value = "MICROSERVICECLOUD-PROVIDERE-DEPT")
+public interface DeptClientService {
+
+    @GetMapping(value = "/dept/get/{id}")
+    public Dept get(@PathVariable("id") long id);
+
+    @GetMapping(value = "/dept/list")
+    public List<Dept> list();
+
+    @PostMapping(value = "/dept/add")
+    public boolean add(Dept dept);
+
+}
